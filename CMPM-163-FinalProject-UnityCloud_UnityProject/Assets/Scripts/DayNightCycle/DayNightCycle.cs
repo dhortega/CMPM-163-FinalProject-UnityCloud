90c8cbfd94aa58fda14c6953c4e5bf387f90dbfa
thdo-hub <52941489+thdo-hub@users.noreply.github.com>
2021-03-03 22:35:22 -0800

    created the day and night scene
    It is a script that rotates the direction of the light by the time of day.
    which can be controlled in the inspector.

---
+++ CMPM-163-FinalProject-UnityCloud_UnityProject/Assets/Scripts/DayNightCycle/DayNightCycle.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+    [Header("Time")]
+    [Tooltip("Day Length in Minutes")]
+    [SerializeField]
+    private float _targetDayLength = 13f; //length of day in minutes
+    public float targetDayLength
+    {
+        get
+        {
+            return _targetDayLength;
+        }
+    }
+    [SerializeField]
+    [Range(0f, 1f)] //Time of day from 0 to 1
+    private float _timeOfDay;
+    public float timeOfDay
+    {
+        get
+        {
+            return _timeOfDay;
+        }
+    }
+    [SerializeField]
+    private int _dayNumber = 0; //tracks the days passed
+    public int dayNumber
+    {
+        get
+        {
+            return _dayNumber;
+        }
+    }
+    [SerializeField]
+    private int _yearNumber = 0; //track the years passed
+    public int yearNumber
+    {
+        get
+        {
+            return _yearNumber;
+        }
+    }
+    private float _timeScale = 100f;
+    [SerializeField]
+    private int _yearLength = 100; //number of days in a year
+    public float yearLength
+    {
+        get
+        {
+            return _yearLength;
+        }
+    }
+    public bool pause = false; //pause the day and night cycle without pausing the game
+
+
+    [Header("Sun Light")]
+    [SerializeField]
+    private Transform dailyRotation;
+    [SerializeField]
+    private Light sun;
+    private float intensity;
+    [SerializeField]
+    private float sunBaseIntensity = 1f;
+    [SerializeField]
+    private float sunVariation = 1.5f;
+
+    [SerializeField]
+    private Gradient sunColor;
+
+
+
+
+    private void Update()
+    {
+        if (!pause)
+        {
+            UpdateTimeScale();
+            UpdateTime();
+        }
+
+        AdjustSunRotation();
+        SunIntensity();
+        AdjustSunColor();
+    }
+    private void UpdateTimeScale()
+    {
+        _timeScale = 24 / (_targetDayLength / 60);
+    }
+
+    private void UpdateTime()
+    {
+        _timeOfDay += Time.deltaTime * _timeScale / 86400; //seconds in a day
+        if(_timeOfDay > 1) //new day
+        {
+            _dayNumber++;
+            _timeOfDay -= 1;
+
+            if(_dayNumber > _yearLength) //new year!
+            {
+                _yearNumber++;
+                _dayNumber = 0;
+            }
+        }
+    }
+
+    //rotates the sun daily (and seasonally soon too);
+    private void AdjustSunRotation()
+    {
+        float sunAngle = timeOfDay * 360f;
+        dailyRotation.transform.localRotation = Quaternion.Euler(new Vector3(0f, 0f, sunAngle));
+    }
+
+    private void SunIntensity()
+    {
+        intensity = Vector3.Dot(sun.transform.forward, Vector3.down);
+        intensity = Mathf.Clamp01(intensity);
+
+        sun.intensity = intensity * sunVariation + sunBaseIntensity;
+    }
+
+    private void AdjustSunColor()
+    {
+        sun.color = sunColor.Evaluate(intensity);
+    }
+
+
+}
